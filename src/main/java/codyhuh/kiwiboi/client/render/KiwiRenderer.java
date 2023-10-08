ec28ab73cde2c32d43ec83dd52f78898212eca49
Coda <cody.koelliker@outlook.com>
2023-10-08 02:52:49 -0600

    fix baby kiwi size

--- src/main/java/codyhuh/kiwiboi/client/render/KiwiRenderer.java
+++ src/main/java/codyhuh/kiwiboi/client/render/KiwiRenderer.java
@@ -3,11 +3,13 @@ package codyhuh.kiwiboi.client.render;
 import codyhuh.kiwiboi.KiwiBoi;
 import codyhuh.kiwiboi.common.entities.Kiwi;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
@@ -23,12 +25,11 @@ public class KiwiRenderer extends GeoEntityRenderer<Kiwi> {
     }
 
     @Override
-    public void render(Kiwi entity, float entityYaw, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight) {
-        super.render(entity, entityYaw, partialTick, poseStack, bufferSource, packedLight);
+    public void preRender(PoseStack poseStack, Kiwi animatable, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
 
-        if (entity.isBaby()) {
+        if (animatable.isBaby()) {
             poseStack.scale(0.5F, 0.5F, 0.5F);
         }
     }
-
 }
